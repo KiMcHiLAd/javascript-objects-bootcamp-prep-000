7cee36791926c1ebdb0d71b63896e3f5303a1f1c
Hania Bigo <hrbigos@umich.edu>
2018-07-24 01:00:43 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,4 +9,14 @@ describe('updatePlaylist(playlist, artistName, songTitle)', () => {
       playlist['My Bloody Valentine'] = 'Sometimes'
     })
     
+  function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
   
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+  
+  return playlist
+}
