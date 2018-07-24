530c7ef642ee5121860482c57b17f3d71ca28ada
Hania Bigo <hrbigos@umich.edu>
2018-07-24 01:02:16 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,22 +1,7 @@
 var playlist = {"Talking Heads" : "And She Was"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  
-}
-describe('updatePlaylist(playlist, artistName, songTitle)', () => {
-    before(() => {
-      playlist['Slowdive'] = 'Alison'
-      playlist['My Bloody Valentine'] = 'Sometimes'
-    })
-    
-  function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle
-  
   return playlist
 }
 
-function removeFromPlaylist(playlist, artistName){
-  delete playlist[artistName]
-  
-  return playlist
-}
