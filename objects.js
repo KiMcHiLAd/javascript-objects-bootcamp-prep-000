b4ee2ca38e6d9e8d77104aa7656c3c4edfd4e5e9
Hania Bigo <hrbigos@umich.edu>
2018-07-24 00:54:18 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = 
+
+
+function deleteFromObjectByKey(object, key) { var object1 = object[key] delete object1[key] return object1 }
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
+  
+  return playlist
+}
