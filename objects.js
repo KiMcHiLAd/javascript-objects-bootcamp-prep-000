94de300be693fa71ce90c5030a461c05855abedd
Hania Bigo <hrbigos@umich.edu>
2018-07-24 00:55:53 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,1 @@
-var playlist = {"Talking Heads":
-
-
-function deleteFromObjectByKey(object, key) { var object1 = object[key] delete object1[key] return object1 }
-
-function updatePlaylist(playlist, artistName, songTitle){
-  playlist[artistName] = songTitle
-  
-  return playlist
-}
+var playlist = {"Talking Heads" : "And She Was"}
