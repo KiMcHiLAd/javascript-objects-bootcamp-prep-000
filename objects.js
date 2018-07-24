813c296f4396d0f0aa8cb6e707d9007caef6d823
Hania Bigo <hrbigos@umich.edu>
2018-07-24 01:00:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,2 +1,12 @@
 var playlist = {"Talking Heads" : "And She Was"}
 
+function updatePlaylist(playlist, artistName, songTitle) {
+  
+}
+describe('updatePlaylist(playlist, artistName, songTitle)', () => {
+    before(() => {
+      playlist['Slowdive'] = 'Alison'
+      playlist['My Bloody Valentine'] = 'Sometimes'
+    })
+    
+  
